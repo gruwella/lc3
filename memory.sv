34aa15c23f021828bbb0436e45a177ef855b1fbe
Ammon Gruwell <gruwella@gmail.com>
2016-12-13 17:30:21 -0700

    fixed mem size

--- memory.sv
+++ memory.sv
@@ -6,7 +6,7 @@ module memory(memOut, clk, reset, memWE, mdrOut, MARReg);
 	//MAR MAR0(Buss, clk, reset, ldMAR, MARReg);	  
 	//MDR MDR0(Buss, memOut, selMDR, clk, reset, ldMDR, mdrOut);
 
-	logic [15:0] my_memory [0:255];
+	logic [15:0] my_memory [0:65535];
 	assign memOut = my_memory[MARReg]; 	   
 	always @(posedge clk) 
 	begin		  	 
